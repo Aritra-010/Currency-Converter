eb969693b4b731f76b89c5683e5f4b4dfc581754
Aritra-010 <iamaritra010@gmail.com>
2026-05-28 14:31:28 +0530

    currency converter

---
+++ script.js
@@ -0,0 +1,64 @@
+const fromSelect = document.getElementById("fromCurrency");
+const toSelect = document.getElementById("toCurrency");
+const amountInput = document.getElementById("amount");
+const result = document.getElementById("result");
+
+async function loadCurrencies() {
+    const res = await fetch("https://api.frankfurter.app/currencies");
+    const data = await res.json();
+
+    // put INR first
+    if (data["INR"]) {
+        const optFrom = document.createElement("option");
+        optFrom.value = "INR";
+        optFrom.textContent = data["INR"];
+        fromSelect.appendChild(optFrom);
+        const optTo = document.createElement("option");
+        optTo.value = "INR";
+        optTo.textContent = data["INR"];
+        toSelect.appendChild(optTo);
+    }
+
+    // add remaining currencies
+    for (const code in data) {
+        if (code === "INR") continue;
+
+        const opt1 = document.createElement("option");
+        opt1.value = code;
+        opt1.textContent = data[code];
+
+        const opt2 = document.createElement("option");
+        opt2.value = code;
+        opt2.textContent = data[code];
+
+        fromSelect.appendChild(opt1);
+        toSelect.appendChild(opt2);
+    }
+}
+
+async function convertCurrency() {
+    const amount = amountInput.value;
+    const from = fromSelect.value;
+    const to = toSelect.value;
+
+    if (!amount || !from || !to) {
+        result.textContent = "";
+        return;
+    }
+    if (from === to) {
+        result.textContent = amount;
+        return;
+    }
+    const res = await fetch(
+        `https://api.frankfurter.app/latest?amount=${amount}&from=${from}&to=${to}`
+    );
+    const data = await res.json();
+
+    result.textContent = data.rates[to];
+}
+
+toSelect.addEventListener("change", convertCurrency);
+fromSelect.addEventListener("change", convertCurrency);
+amountInput.addEventListener("input", convertCurrency);
+
+loadCurrencies();
