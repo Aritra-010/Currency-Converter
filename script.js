e65fb575512f189766a189df00a15af619ee8304
Aritra Das <iamaritra010@gmail.com>
2026-08-30 01:53:43 +0530

    Add Firebase Google authentication

--- script.js
+++ script.js
@@ -1,3 +1,74 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
+import {
+    getAuth,
+    GoogleAuthProvider,
+    signInWithPopup,
+    onAuthStateChanged,
+    signOut
+} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-auth.js";
+const firebaseConfig = {
+    apiKey: "AIzaSyCKUCCbRo-OvnpE4pNOx9bX3-j8yPB1oao",
+    authDomain: "currency-converter-d098c.firebaseapp.com",
+    projectId: "currency-converter-d098c",
+    storageBucket: "currency-converter-d098c.firebasestorage.app",
+    messagingSenderId: "739677937464",
+    appId: "1:739677937464:web:787ebb24102c4af920abcd"
+  };
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+const googleLoginBtn = document.getElementById("googleLoginBtn");
+const logoutBtn = document.getElementById("logoutBtn");
+const userInfo = document.getElementById("userInfo");
+
+googleLoginBtn.addEventListener("click", async () => {
+    try {
+        const result = await signInWithPopup(auth, provider);
+
+        const user = result.user;
+
+        console.log("Logged in user:", user);
+
+        userInfo.textContent = `Welcome, ${user.displayName}`;
+    } catch (error) {
+        console.error("Login error:", error);
+    }
+});
+
+logoutBtn.addEventListener("click", async () => {
+    try {
+        await signOut(auth);
+        console.log("User logged out");
+    } catch (error) {
+        console.error("Logout error:", error);
+    }
+});
+
+onAuthStateChanged(auth, (user) => {
+
+    if (user) {
+        console.log("User is logged in:", user.email);
+
+        googleLoginBtn.style.display = "none";
+        logoutBtn.style.display = "inline-block";
+
+        userInfo.textContent = `Welcome, ${user.displayName}`;
+
+    } else {
+        console.log("No user is logged in");
+
+        googleLoginBtn.style.display = "inline-block";
+        logoutBtn.style.display = "none";
+
+        userInfo.textContent = "";
+    }
+
+});
+
+console.log("Firebase initialized successfully!");
+
 const fromSelect = document.getElementById("fromCurrency");
 const toSelect = document.getElementById("toCurrency");
 const amountInput = document.getElementById("amount");
